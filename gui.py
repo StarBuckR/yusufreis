ff2cb65f1fc7399dc5a5020b92406d63da1a536b
StarBuckR <ridvantulemen@gmail.com>
2020-07-14 20:28:35 +0300

    recreated send window and hotfixes

--- gui.py
+++ gui.py
@@ -11,25 +11,46 @@ el = gettext.translation('base', 'locale', fallback=True)
 el.install()
 _ = el.gettext
 
-class Window(Gtk.Builder):
+class Window(object):
     def __init__(self):
-        Gtk.Builder.__init__(self)
-        builder = Gtk.Builder()
-        builder.add_from_file("popup.glade")
-
-        note_label = builder.get_object("note")
-        note_label.set_label(_("Note"))
-
-        send_button = builder.get_object("send_button")
-        send_button.set_label(_("Send"))
-
-        self.image_window = builder.get_object("image")
-        self.textview = builder.get_object("textview")
-        
-        send_button.connect("clicked", self.on_send_clicked)
-        
-        self.window = builder.get_object("popup_window")
+        self.window = Gtk.Window(type=Gtk.WindowType.TOPLEVEL)
         self.window.set_title(_("Send"))
+        self.window.set_position(Gtk.WindowPosition.CENTER_ALWAYS)
+        self.window.set_border_width(32)
+        self.window.set_default_size(400, 400)
+        self.window.set_resizable(False)
+
+        grid = Gtk.Grid()
+        grid.set_row_spacing(5)
+        grid.set_column_spacing(5)
+        grid.set_halign(Gtk.Align.CENTER)
+        grid.set_direction(Gtk.TextDirection.LTR)
+
+        self.window.add(grid)
+
+        self.textview = Gtk.TextView()
+        self.textview.set_size_request(400, 200)
+        textbuffer = self.textview.get_buffer()
+        textbuffer.set_text(_("Start typing your note here"))
+
+        label = Gtk.Label(_("Note:"))
+        label.set_direction(Gtk.TextDirection.LTR)
+        label.set_halign(Gtk.Align.CENTER)
+
+        send_button = Gtk.Button(label=_("Send"))
+        send_button.set_size_request(80, 30)
+        send_button.connect("clicked", self.on_send_click)
+
+        cancel_button = Gtk.Button(label=_("Cancel"))
+        cancel_button.set_size_request(80, 30)
+        cancel_button.connect("clicked", self.on_cancel_click)
+
+        grid.attach(label, 50, 10, 4, 1)
+        grid.attach_next_to(self.textview, label, Gtk.PositionType.BOTTOM, 100, 30)
+        grid.attach_next_to(send_button, self.textview, Gtk.PositionType.BOTTOM, 50, 10)
+        grid.attach_next_to(cancel_button, send_button, Gtk.PositionType.RIGHT, 50, 10)
+        
+        self.window.show_all()
         self.window.connect("destroy", self.on_destroy)
 
     def show_popup_window(self):
@@ -45,7 +66,7 @@ class Window(Gtk.Builder):
         # self.image_window.set_from_pixbuf(pixbuf)
         self.window.show_all()
 
-    def on_send_clicked(self, button):
+    def on_send_click(self, button):
         buffer = self.textview.get_buffer()
         text = buffer.get_text(buffer.get_start_iter(), buffer.get_end_iter(), False)
         print(text)
@@ -69,6 +90,9 @@ class Window(Gtk.Builder):
         #response = requests.get('http://localhost:3000/users')
         print(response.text)
 
+        self.window.close()
+
+    def on_cancel_click(self, button):
         self.window.close()
 
     def on_destroy(self, window):
